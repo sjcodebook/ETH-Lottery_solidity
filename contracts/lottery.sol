adfd52bbb132132704f0f41427a6232c99d9d668
0xSJ <jainsanmati846@gmail.com>
2022-11-06 02:40:05 +0530

    Add chainlinkVRF

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -2,13 +2,19 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Lottery__NotEnoughETHEntered();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     uint256 private immutable i_entrancefee;
     address payable[] private s_players;
 
-    constructor(uint256 _entrancefee) {
+    event Lottery__PlayerEntered(address player, uint256 amount);
+
+    constructor(address vrfCoordinatorV2, uint256 _entrancefee)
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_entrancefee = _entrancefee;
     }
 
@@ -17,7 +23,15 @@ contract Lottery {
             revert Lottery__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit Lottery__PlayerEntered(msg.sender, msg.value);
     }
+
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entrancefee;
